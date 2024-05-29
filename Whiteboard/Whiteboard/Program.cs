211fed6f192beb07917853eaf4c547cccb2abc4f
Cmarsman <chris_marsden@yahoo.com>
2024-05-28 19:30:59 -0500

    Whiteboard exercise

---
+++ Whiteboard/Whiteboard/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.Design;
+
+namespace Whiteboard
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(10));
+        }
+        public static string FizzBuzz(int num) 
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if (num % 3 == 0)
+            {
+                return "fizz";
+            }
+            else if (num % 5 == 0)
+            {
+                return "buzz";
+            }
+            else 
+            {
+                return num.ToString();
+            }
+            
+
+        }
+
+    }
+}
